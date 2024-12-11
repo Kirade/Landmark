7976a749a166541a25297d7956561c197511a6bc
JaeyoungHeo <jay.jaeyoung@gmail.com>
2024-12-11 13:35:56 +0900

    feat: SwiftUI Tutorial - Building lists and navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 허재영 on 2024.12.11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
